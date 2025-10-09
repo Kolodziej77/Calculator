acb08b4c8c65d7a8741922f542f97b6acaacba19
Kolodziej7 <206709858+Kolodziej77@users.noreply.github.com>
2025-10-09 14:04:52 +0200

    Implement display update for number buttons

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const display = document.getElementById('result');
 const historyDisplay = document.getElementById('history');
 
+let current = '';
+let previous = '';
+let operator = null;
+
 function add(a, b){
     return a + b;
 }
@@ -18,4 +22,25 @@ function divide(a, b){
         return 'Error! Do not divide by 0!';
     }
     return a / b;
+}
+
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', () => {
+        console.log(button.textContent);
+        if(button.classList.contains('number')){
+            appendNumber(button.textContent);
+        }
+    })
+})
+
+function updateDisplay(){
+    display.textContent = current;
+}
+
+function appendNumber(num){
+    if(num === '.' && current.includes('.')){
+        return;
+    }
+    current += num;
+    updateDisplay();
 }
